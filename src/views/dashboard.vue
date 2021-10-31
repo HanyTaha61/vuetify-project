380fb0b79e1da2c6953e3bdf8b4cafb30b6d4cba
HanyTaha61 <hanygrand1980@gmail>
2021-10-31 10:37:59 +0200

    layout of edit and delete buttons completed

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -23,38 +23,62 @@
         </v-tooltip>
       </v-layout>
       <v-card flat :class="`grey lighten-3 mb-1 border project ${project.status}`" v-for="(project, i) in projectsArray" :key="i" :id="project.id">
+        <div class="edit-list">
+          <button @click="deleteProject(project.id)" class="deleteProject small mx-1 grey v-btn v-btn--outlined v-btn--text theme--dark v-size--default">
+            <v-icon left>mdi-delete</v-icon>
+            delete
+          </button>
+          <v-btn dark @click.stop="dialog = true" class="modifyProject small mx-1 grey v-btn v-btn--outlined v-btn--text theme--dark v-size--default">
+            <v-icon left>mdi-pencil</v-icon>
+            edit
+          </v-btn>
+        </div>
         <v-layout class="no-gutters pa-1" wrap row>
-          <v-flex xs10 md4 class="primary--text">
+          <v-flex xs12 sm4 md3 class="px-2  primary--text">
             <div class="caption primary--text pa-1 font-weight-bold">Project Title</div>
             <div class="black--text">{{ project.title }}</div>
-            <article>{{ project.content }}</article>
+            <div class="red--text caption">ID: {{ project.id }}</div>
           </v-flex>
-          <v-flex xs6 sm4 md2 class="primary--text">
+          <v-flex xs12 sm8 md3 class="px-2  primary--text">
+            <div class="caption primary--text pa-1 font-weight-bold">Project content</div>
+            <div class="black--text">{{ project.content }}</div>
+          </v-flex>
+          <v-flex xs12 sm4 md2 class="px-2  primary--text">
             <div class="caption primary--text pa-1 font-weight-bold">Person</div>
             <div class="black--text">{{ project.person }}</div>
           </v-flex>
-          <v-flex xs6 sm4 md2 class="primary--text">
+          <v-flex xs12 sm4 md2 class="px-2  primary--text">
             <div class="caption primary--text pa-1 font-weight-bold">Due date</div>
             <div class="black--text">{{ project.due }}</div>
           </v-flex>
-          <v-flex xs2 sm4 md1 class="primary--text">
-              <div>
+          <v-flex xs12 sm4 md2 class="px-md-2 primary--text">
+              <div class="caption primary--text pa-1 font-weight-bold">Project status
                 <v-chip small :class="`${project.status} dark--text caption mt-4 px-3 mr-5`">
                     {{project.status}}
                 </v-chip>
               </div>
           </v-flex>
-          <v-flex xs6 sm4 md3 class="primary--text">
-            <div class="caption primary--text pa-1 font-weight-bold">
+        </v-layout>
+      </v-card>
+
+<!-- -------------------------------------------- -->
+
+
+
+<!-- -------------------------------------------- -->
+
+
+          <!-- <v-flex xs6 sm4 md3 class="primary--text">
+            <div class="caption primary--text pa-1 font-weight-bold"> -->
 
               <!-- ******************* -->
               <!-- START DELETE BUTTON -->
               <!-- ******************* -->
 
-                <button @click="deleteProject(project.id)" class="deleteProject small mx-1 grey v-btn v-btn--outlined v-btn--text theme--dark v-size--default">
+                <!-- <button @click="deleteProject(project.id)" class="deleteProject small mx-1 grey v-btn v-btn--outlined v-btn--text theme--dark v-size--default">
                   <v-icon left>mdi-delete</v-icon>
                   delete
-                </button>
+                </button> -->
               
               <!-- ***************** -->
               <!-- END DELETE BUTTON -->
@@ -66,50 +90,18 @@
               <!-- START EDIT BUTTON -->
               <!-- ***************** -->
 
-                  <v-dialog persistent :retain-focus="false" width="500" v-model="dialog" >
-                    <template v-slot:activator="{ on, attrs }">
-                      <v-btn v-bind="attrs" v-on="on" text outlined class="grey modifyProject" dark @click="modifyProject(project.id)">
-                              <v-icon left>mdi-pencil</v-icon>
-                        edit
-                      </v-btn>
-                    </template>
-                    <v-card>
-                      <v-card-title class="text-h5 text-uppercase grey lighten-2">
-                        <div class="w-fit mx-auto">edit project</div>
-                      </v-card-title>
-                      <v-card-text>
-                          <v-form ref="form">
-                            <v-text-field label="project title" :value="project.title" prepend-icon="mdi-folder"></v-text-field>
-                            <v-textarea label="project content" :value="project.content" prepend-icon="mdi-pencil"></v-textarea>
-                              <v-menu ref="menu" v-model="menu" :close-on-content-click="false" :return-value.sync="date" transition="scale-transition" offset-y  min-width="auto" >
-                              <template v-slot:activator="{ on, attrs }">
-                              <v-text-field v-model="date" label="project due date" prepend-icon="mdi-calendar" readonly v-bind="attrs" v-on="on" ></v-text-field>
-                              </template>
-                              <v-date-picker v-model="due" no-title scrollable >
-                              <v-spacer></v-spacer>
-                              <v-btn text color="primary" @click="menu = false" >Cancel</v-btn>
-                              <v-btn text color="primary" @click="$refs.menu.save(date)" >OK</v-btn>
-                              </v-date-picker>
-                          </v-menu>   
-                          </v-form>
-                      </v-card-text>
-                      <v-divider></v-divider>
-                      <v-card-actions class="justify-start">
-                        <v-btn :loading='loading' class="grey lighten-2 text--darken-4" color="grey" outlined @click="submit()" >Apply</v-btn>
-                        <v-btn class="grey lighten-2 text--darken-4" color="grey" outlined @click="cancel()" >Cancel</v-btn>
-                      </v-card-actions>
-                      
-                    </v-card>
-                  </v-dialog>
-
-              <!-- *************** -->
-              <!-- END EDIT BUTTON -->
-              <!-- *************** -->
-              
-            </div>
-          </v-flex>
-        </v-layout>
-      </v-card>
+    <!-- <v-btn dark @click.stop="dialog = true" class="modifyProject small mx-1 grey v-btn v-btn--outlined v-btn--text theme--dark v-size--default">
+      <v-icon left>mdi-pencil</v-icon>
+      edit
+    </v-btn> -->
+            <!-- *************** -->
+            <!-- END EDIT BUTTON -->
+            <!-- *************** -->
+ 
+            <!-- </div>
+          </v-flex> -->
+        <!-- </v-layout> -->
+      
     </v-container>
   </div>
 </template>
@@ -121,15 +113,19 @@ import { collection, query, onSnapshot, deleteDoc, doc } from "firebase/firestor
 export default {
   data: ()=>{
     return{
+      absolute: false,
+      overlay: false,
       projectsArray: [],
-      dialog: false,
+      dialog: true,
       title: '',
       content: '',
-      due: null,
+      due: '',
       date: (new Date(Date.now() - (new Date()).getTimezoneOffset() * 60000)).toISOString().substr(0, 10),
       menu: false,
       loading: false,
-      
+      updatedTitle: '',
+      updatedContent: '',
+      updatedDue: ''
     }
   },
   methods:{
@@ -141,8 +137,15 @@ export default {
      await deleteDoc(doc(db, "Projects", id))
       console.log(id + ' ---- was deleted -----');
     },
-    modifyProject(id){
+   async modifyProject(id){
+      // const selectedProject = doc(db, "Projects", id);
+
       console.log(id);
+      // await updateDoc(selectedProject, {
+      //   title: 'new title',
+      //   content: 'new content',
+      //   due: '12-12-2012'
+      // });
     },
     cancel(){
       this.title = '',
@@ -182,6 +185,7 @@ export default {
   .border{
     border: 1px solid #000 !important;
   }
+  .project{position: relative;}
   .project.completed{
     border-left: 5px solid #12be6e !important;
   }
@@ -232,5 +236,32 @@ export default {
 .modifyProject:hover, .modifyProject:hover .v-icon.mdi-pencil{
   color: #12be6e;
   background-color: transparent !important;
+}
+.v-card--reveal {
+  align-items: center;
+  bottom: 0;
+  justify-content: center;
+  opacity: 0.85;
+  position: absolute;
+  width: 100%;
+}
+.project .edit-list{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  color: #fff;
+  opacity: 0;
+  content: '';
+  z-index: 2;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+  top: 0;
+  left: 0;
+  background-color: rgba(0,0,0,0.5);
+  transition: 0.25s;
+}
+.project:hover .edit-list{
+  opacity: 1;
 }
 </style>
